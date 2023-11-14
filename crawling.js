b34c4e3a93340866be494b0174dbd0c413f39142
rovannoufer <noufermohamed007@gmail.com>
2023-11-14 16:22:34 +0530

    Getting URL from HTMl

--- crawling.js
+++ crawling.js
@@ -1,3 +1,41 @@
+const { JSDOM } = require('jsdom')
+
+
+
+
+function geturlfromhtml(html_body, base_url){
+    const urls = []
+    const dom = new JSDOM(html_body)
+    const link_elements = dom.window.document.querySelectorAll('a')
+    for(const link_element of link_elements){
+       //relative
+        if(link_element.href.slice(0,1) === '/'){
+           try{
+            const urlobj = new URL(`${base_url}${link_element.href}`)
+            urls.push(urlobj.href)
+           }
+           catch(err){
+            console.log(`Error : ${err.message}`)
+           }
+        }
+
+        //absolute
+        else{
+           try{
+            const urlobj = new URL(link_element.href)
+            urls.push(urlobj.href)
+           }catch(err){
+            console.log(`Error : ${err.message}`)
+           }
+        }
+        
+    }
+
+    return urls
+}
+
+
+
 function normalizeURL(url_string){
     const url_object = new URL(url_string)
     host_path =  `${url_object.hostname}${url_object.pathname}`
@@ -11,5 +49,6 @@ function normalizeURL(url_string){
 
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    geturlfromhtml
 }
--- crawling.test.js
+++ crawling.test.js
@@ -1,27 +1,98 @@
-const { normalizeURL } = require('./crawling')
+const { normalizeURL, geturlfromhtml } = require('./crawling.js')
 const { test, expect } = require('@jest/globals')
 
 
 test('normalizeURL strip protocol', () =>{
-    const input = 'https://blog.boot.dev/path'
+    const input = 'https://www.google.com'
     const actual_output = normalizeURL(input)
-    const expected_output = 'blog.boot.dev/path'
+    const expected_output = 'www.google.com'
     expect(actual_output).toEqual(expected_output)
 })
 
 
 test('normalizeURL strip protocol slash', () =>{
-    const input = 'https://blog.boot.dev/path/'
+    const input = 'https://www.google.com/'
     const actual_output = normalizeURL(input)
-    const expected_output = 'blog.boot.dev/path'
+    const expected_output = 'www.google.com'
     expect(actual_output).toEqual(expected_output)
 })
 
 
 
 test('normalizeURL strip capital', () =>{
-    const input = 'https://BLOG.Boot.dev/path'
+    const input = 'https://www.GOOGLE.com'
     const actual_output = normalizeURL(input)
-    const expected_output = 'blog.boot.dev/path'
+    const expected_output = 'www.google.com'
+    expect(actual_output).toEqual(expected_output)
+})
+
+
+
+test('geturlfromhtml', () =>{
+    const input = `
+    <html>
+       <body>
+       <a href="https://www.google.com/path/">Google</a>
+       </body>
+    </html> 
+    `
+
+    const input_base_url = "https://www.google.com/path/"
+    const actual_output = geturlfromhtml(input, input_base_url)
+    const expected_output = ["https://www.google.com/path/"]
+    expect(actual_output).toEqual(expected_output)
+})
+
+
+test('geturlfromhtml relativeurls', () =>{
+    const input = `
+    <html>
+       <body>
+       <a href="/path/">Google</a>
+       </body>
+    </html> 
+    `
+
+    const input_base_url = "https://www.google.com"
+    const actual_output = geturlfromhtml(input, input_base_url)
+    const expected_output = ["https://www.google.com/path/"]
+    expect(actual_output).toEqual(expected_output)
+})
+
+
+
+
+test('geturlfromhtml multipleurls', () =>{
+    const input = `
+    <html>
+       <body>
+       <a href="https://www.google.com/path1/">Google path1</a>
+       <a href="/path2/">Google path2</a>
+       <a href="/path3/">Google path3</a>
+       </body>
+    </html> 
+    `
+
+    const input_base_url = "https://www.google.com"
+    const actual_output = geturlfromhtml(input, input_base_url)
+    const expected_output = ["https://www.google.com/path1/", "https://www.google.com/path2/", "https://www.google.com/path3/"]
+    expect(actual_output).toEqual(expected_output)
+})
+
+
+
+
+test('geturlfromhtml invalidurls', () =>{
+    const input = `
+    <html>
+       <body>
+       <a href="invalid url">--In--valid--</a>
+       </body>
+    </html> 
+    `
+
+    const input_base_url = "https://www.google.com"
+    const actual_output = geturlfromhtml(input, input_base_url)
+    const expected_output = []
     expect(actual_output).toEqual(expected_output)
 })
